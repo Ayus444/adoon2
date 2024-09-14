58c1f18df37e66afaf60560da1f61f52033cd92f
Unique <mujeem000@gmail.com>
2024-09-14 08:13:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ except ValueError:
 ADMINS.append(OWNER)
 
 bot = Client("bot",    
-   bot_token="7406875919:AAHGJB-GFD3hhJwWJqq_KnVSL9BwbizWPbc",    
+   bot_token="7341057146:AAGW0VvAGgDGPRhzJTQTLIiwyljrhT5Mxgo",    
    api_id= 21179966,    
    api_hash= "d97919fb0a3c725e8bb2a25bbb37d57c"
 )
